fec9f845abcd318ebea2d8d33d6e34bc9974b069
Charles Furtado <charlees1509@hotmail.com>
2023-03-08 15:28:33 -0300

    Improve Survey.

--- lib/survey.ex
+++ lib/survey.ex
@@ -1,38 +1,16 @@
 defmodule CraqQuestions.Survey do
   @moduledoc """
-  Module for handling the answers for the questions
+  Module for handling the answers for the given questions
   """
 
   alias CraqQuestions.Question
 
-  @questions [
-    {
-      %Question{
-        text: "What is the best way to play guitar?",
-        options: [%{text: "by myself"}, %{text: "with lesson paid"}]
-      },
-      %Question{
-        text: "What is your favorite guitar branch?",
-        options: [%{text: "Taylor"}, %{text: "Gibson"}, %{text: "Seizi"}, %{text: "Martin"}]
-      },
-      %Question{
-        text: "What is the grunge band the influenced the most?",
-        options: [%{text: "Nirvana"}, %{text: "Alice in chains"}, %{text: "Pearl Jam"}]
-      },
-      %Question{
-        text: "What is the best single below",
-        options: [
-          %{text: "Everlong", completed_if_selected: true},
-          %{text: "The pretender"},
-          %{text: "Wheels"},
-          %{text: "Best of you"}
-        ]
-      }
-    }
-  ]
+  def validate_answers(_questions, answers) when answers == %{} do
+    "was not answered"
+  end
 
-  def validate_answers(answers) do
-    questions = Enum.with_index(@questions)
+  def validate_answers(questions, answers) do
+    questions = Enum.with_index(questions)
     errors = %{errors: []}
 
     result =
@@ -42,14 +20,14 @@ defmodule CraqQuestions.Survey do
         if answer_value = Map.get(answers, answer_key) do
           acc
           |> validate_answer(question, answer_value, answer_key)
-          |> valid_answer_if_terminal(questions, question_number, answer_key)
+          |> valid_answer_if_terminal(questions, answers, question_number, answer_key)
         else
-          acc[:errors] ++ [[answer_key, "was not answered"]]
+          Map.put(acc, :errors, acc.errors ++ [{answer_key, "was not answered"}])
         end
       end)
 
-    if length(result[:errors]) > 0 do
-      result[:errors]
+    if length(result.errors) > 0 do
+      result.errors
     else
       :ok
     end
@@ -61,22 +39,38 @@ defmodule CraqQuestions.Survey do
     if Enum.any?(options_with_index, fn {_option, index} -> index == answer_value end) do
       acc
     else
-      acc[:errors] ++ [{answer_key, "has an answer that is not on the list of valid answers"}]
+      Map.put(
+        acc,
+        :errors,
+        acc.errors ++ [{answer_key, "has an answer that is not on the list of valid answers"}]
+      )
     end
   end
 
-  defp valid_answer_if_terminal(acc, questions, question_number, answer_key) do
-    {%Question{options: options}, _index} =
-      Enum.find(questions, fn {_question, index} -> index == question_number - 1 end)
+  defp valid_answer_if_terminal(acc, questions, answers, question_number, answer_key) do
+    previous_answer_key = ("q" <> "#{question_number - 1}") |> String.to_atom()
+    previous_answer_value = Map.get(answers, previous_answer_key)
 
-    if Enum.any?(options, fn option -> option.complete_if_selected end) do
-      acc[:errors] ++
-        [
-          {answer_key,
-           "was answered even though a previous response indicated that the questions were complete"}
-        ]
+    with {%Question{options: options}, _index} <-
+           Enum.find(questions, fn {_question, index} -> index == question_number - 1 end),
+         options_with_index <- Enum.with_index(options),
+         true <-
+           Enum.any?(options_with_index, fn {option, index} ->
+             Map.has_key?(option, :completed_if_selected) and option.completed_if_selected and
+               index == previous_answer_value
+           end) do
+      Map.put(
+        acc,
+        :errors,
+        acc.errors ++
+          [
+            {answer_key,
+             "was answered even though a previous response indicated that the questions were complete"}
+          ]
+      )
     else
-      acc
+      _error ->
+        acc
     end
   end
 end
